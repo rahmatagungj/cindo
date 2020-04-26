703418148204a96b59096daacaaf7458ab9cb483
rahmatagungj <rahmatagungj@gmail.com>
2020-04-26 22:35:15 +0700

    Commit Cindo

---
+++ CINDO.h
@@ -0,0 +1,66 @@
+/*
+ * C++ Bahasa Indonesia
+ *
+ *	Dibuat Oleh : Rahmat Agung Julians
+ *	26 April 2020
+ *
+ */
+ 
+ #ifndef INDONESIA_H_
+ #define INDONESIA_H_
+ 
+ /* Mulai */
+ #define std using namespace std;
+ 
+ /* Tampilan */
+ #define tampilkan cout<<
+ #define masukan cin>>
+ #define garisbaru cout<<"\n"
+ #define garis_baru \n
+ 
+ /* Tipe Data */
+ #define nomor int
+ #define karakter char
+ #define desimal float
+ #define teks string
+ 
+ /* Main Fungsi dan Kondisi*/
+ #define untuk for
+ #define jika if
+ #define jika_tidak else 
+ #define atau else if
+ #define ketika while
+
+/* nilai */
+ #define ditambah +
+ #define dikurangi -
+ #define tambah +
+ #define kurang -
+ #define bertambah ++
+ 	#define bertambah_1 	-1
+ 	#define bertambah_2		-2
+ 	#define bertambah_3		-3
+ 	#define bertambah_4		-4
+ 	#define bertambah_5		-5
+ 	#define bertambah_6		-6
+ 	#define bertambah_7		-7 
+ 	#define bertambah_8		-8
+ 	#define bertambah_9		-9
+ 	#define bertambah_10	-10
+ #define berkurang -1
+ 	#define berkurang_1 	-1
+ 	#define berkurang_2		-2
+ 	#define berkurang_3		-3
+ 	#define berkurang_4		-4
+ 	#define berkurang_5		-5
+ 	#define berkurang_6		-6
+ 	#define berkurang_7		-7 
+ 	#define berkurang_8		-8
+ 	#define berkurang_9		-9
+ 	#define berkurang_10	-10
+
+/* kontrol sistem */
+ #define dan <<
+
+
+ #endif /* INDONESIA_H_ */
---
+++ contoh.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "cindo.h"
+std;
+
+int main(){
+	nomor a=10,b;
+	karakter nama[10];
+	tampilkan "Masuka nama kamu : "; masukan nama;
+	tampilkan "halo " dan nama;
+}
